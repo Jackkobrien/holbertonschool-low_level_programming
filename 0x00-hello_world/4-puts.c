b9c77138f60dea3c7d7bbd04b1490a625ef7829f
Jackkobrien <2104@holbertonschool.com>
2020-09-17 17:58:21 +0000

    Automated commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
-int main(void)
+C#include <stdio.h>
+/**
+ * main - 0x00 Task 4
+ */
+void main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  getchar();
-  return(0)
+puts("Programming is like building a multilingual puzzle");
+getchar();
 }

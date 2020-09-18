76182fdfb635ae08bd94db49152e5d61e33c6247
Jackkobrien <2104@holbertonschool.com>
2020-09-18 16:30:58 +0000

    Automated commit message

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+  int a;
+  int b;
+  int c;
+  int d;
+  int e;
+
+  a = sizeof(char);
+  b = sizeof(int);
+  c = sizeof(long int);
+  d = sizeof(long long int);
+  e = sizeof(float);
+
+  printf("Size of a char: %d byte(s)\n",a);
+  printf("Size of an int: %d byte(s)\n",b);
+  printf("Size of a long int: %d byte(s)\n",c);
+  printf("Size of a long long int: %d byte(s)\n",d);
+  printf("Size of a float: %d byte(s)\n",e);
+}

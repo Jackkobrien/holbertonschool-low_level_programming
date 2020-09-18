c198fa077f660e488f0f3ff9f30025c2c1764fb2
Jackkobrien <2104@holbertonschool.com>
2020-09-18 18:30:54 +0000

    Automated commit message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main- prints the alphabet in lowercase
+ *
+ * Return: nothing
+ */
+
+int main(void)
+{
+	int i;
+	char alphabet[27];
+
+	i = 0;
+	alphabet[0] = 'a';
+
+	for (i = 0; i < 27; i++)
+{
+	if (i == 26)
+		alphabet[i] = '\n';
+
+		putchar(alphabet[i]);
+		alphabet[i + 1] = alphabet[i] + 1;
+}
+}

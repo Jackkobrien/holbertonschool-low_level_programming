319dd16557c35c38fde2f8becdc80aa2dae43624
Jackkobrien <2104@holbertonschool.com>
2020-09-25 19:11:53 +0000

    Automated commit message

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Prints hex numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	int f;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+
+	for (f = 'a'; f <= 'f'; f++)
+		putchar(f);
+
+	putchar('\n');
+}

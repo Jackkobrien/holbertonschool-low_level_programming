97237851d8f8b262eacb915df069ee285afe47ce
Jackkobrien <2104@holbertonschool.com>
2020-09-29 18:52:50 +0000

    Automated commit message

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  *puts_half - prints seconds half of a function
  *@str: a string
@@ -12,17 +13,17 @@ void puts_half(char *str)
 
 	len = 0;
 
-	while (str[len] != '\0' && str[len + 1] != '\0')
-	{
+	while (str[len] != '\0')
 		len++;
-	}
+
 	if (len % 2 == 0)
 		n = len / 2;
 	else if (len % 2 != 0)
 		n = (len - 1) / 2;
 	while (n <= len)
 	{
-		_putchar[str[n]];
+		_putchar(str[n]);
 		n++;
 	}
+	_putchar('\n');
 }

bdd02c951dd0b3526be27609db0a9f8186778f50
Jackkobrien <2104@holbertonschool.com>
2020-09-29 16:33:33 +0000

    Automated commit message

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string s in reverse
+ *@s: pointer to a string s
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	while (len >= 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+	_putchar('\n');
+}

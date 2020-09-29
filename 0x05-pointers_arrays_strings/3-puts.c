5ad50a359d3fd66ffc3c4813a41f751dc6e5e5bf
Jackkobrien <2104@holbertonschool.com>
2020-09-29 14:32:31 +0000

    Automated commit message

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "holberton.h"
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,10 +1,15 @@
-#include <stdio.h>
 #include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char *str;
 
-	str = "Holberton!";
-	printf("%p\n", &str);
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
 	return (0);
 }

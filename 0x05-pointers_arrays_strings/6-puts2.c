a9907a118441c2b55a62f9ab67fdbcb318a6502c
Jackkobrien <2104@holbertonschool.com>
2020-09-29 18:05:15 +0000

    Automated commit message

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,17 @@ void puts2(char *str)
 {
 	int len;
 
-	len = 1;
+	len = 0;
 
-	_putchar(str[0]);
 	while (str[len] != '\0' && str[len + 1] != '\0')
 	{
+		if (len == 0)
+		{
+			_putchar(str[0]);
+			len++;
+			continue;
+		}
+
 		if (len % 2 == 0)
 			_putchar(str[len]);
 		len++;

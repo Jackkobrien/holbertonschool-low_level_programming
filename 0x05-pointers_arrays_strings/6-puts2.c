c96f07fcba8658aacd7badd07e285355aa285022
Jackkobrien <2104@holbertonschool.com>
2020-09-29 17:57:01 +0000

    Automated commit message

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character of a string str
+ *@str: a pointer to a string
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int len;
+
+	len = 1;
+
+	_putchar(str[0]);
+	while (str[len] != '\0' && str[len + 1] != '\0')
+	{
+		if (len % 2 == 0)
+			_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}

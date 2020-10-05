4e097e3c311ae237f6d1b60a4d4450b99624caa4
Jackkobrien <2104@holbertonschool.com>
2020-10-05 18:28:27 +0000

    Automated commit message

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,17 @@
+char *cap_string(char *a)
+{
+	int i = 0;
+	int j = 0;
+	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(',
+			     ')', '{', '}', '\0'};
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			for (j = 0; separators[j] != '\0'; j++)
+				if (a[i - 1] == separators[j] || i == 0)
+					a[i] = a[i] - 32;
+		i++;
+	}
+	return (a);
+}

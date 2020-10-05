2798a5be1fc0dda7cc3c8686a3f397879489500a
Jackkobrien <2104@holbertonschool.com>
2020-10-05 13:52:44 +0000

    Automated commit message

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strncat - cat up to n bytes of str src to str dest
+ *@dest:string to be cat to
+ *@src:string to cat
+ *@n: number of bytes to cat
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = 0;
+
+	while (dest[len] != '\0')
+		len++;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+
+	dest[len + i] = '\0';
+}

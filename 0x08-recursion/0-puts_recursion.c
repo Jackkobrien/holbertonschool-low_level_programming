6f52a50e0b4b0fbcd0962dd786b905e5889482a8
Jackkobrien <2104@holbertonschool.com>
2020-10-07 14:23:31 +0000

    Automated commit message

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *_puits_recursion - prints a function recursively
+ *@s: str to print
+ *Return: null
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion((s + 1));
+}

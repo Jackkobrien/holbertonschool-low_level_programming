8af84025e4d120e67e9bd18e60269f9dd8d724cf
Jackkobrien <2104@holbertonschool.com>
2020-10-08 03:35:00 +0000

    Automated commit message

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,31 @@
 #include "holberton.h"
-int is_prime_number(int n)
+/**
+ *solver - expansion of main fn with 2 variables
+ *@n: n from main fn
+ *@a: 1
+ * Return: 1 if n prime 0 if not
+ */
+int solver(int n, int a)
 {
-	int k;
-
-	if (n < 0)
+	if  (n < 0)
 		n = n * -1;
+	if (n == 0)
+		return (1);
 	if (n == 1)
 		return (0);
-	k = n % (n - 1);
-
-	if (k == 0)
-		return (0);
-	if (k == 0)
+	if (a == 1)
 		return (1);
-	return (is_prime_number(n - 1));
+	if (n % a == 0 && n != a)
+		return (0);
+	return (solver(n, a - 1));
+
+}
+/**
+ *is_prime_number - computes if a number is prime
+ *@n: num to compute
+ * Return: solver
+ */
+int is_prime_number(int n)
+{
+	return (solver(n, n));
 }
